84d961052f158edd3c357c5fe3d9348f9c93f693
Armen Mkhitaryan <armdev19@gmail.com>
2022-07-27 17:36:13 +0400

    Created NoteItem.kt

---
+++ app/src/main/java/com/armdev19/noteapp/feature_note/presentation/notes/components/OrderSection.kt
@@ -0,0 +1,58 @@
+package com.armdev19.noteapp.feature_note.presentation.notes.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.armdev19.noteapp.feature_note.domain.util.NoteOrder
+import com.armdev19.noteapp.feature_note.domain.util.OrderType
+
+@Composable
+fun OrderSection(
+    modifier: Modifier = Modifier,
+    noteOrder: NoteOrder = NoteOrder.Date(orderType = OrderType.Descending),
+    onOrderChange: (NoteOrder) -> Unit
+) {
+    Column(
+        modifier = modifier
+    ) {
+        Row(
+            modifier = modifier.fillMaxHeight()
+        ) {
+            DefaultRadioButton(text = "Title",
+                selected = noteOrder is NoteOrder.Title,
+                onSelect = {
+                    onOrderChange(NoteOrder.Title(orderType = noteOrder.orderType)
+                    )
+                })
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(text = "Date",
+                selected = noteOrder is NoteOrder.Date,
+                onSelect = { onOrderChange(NoteOrder.Date(orderType = noteOrder.orderType)
+                    )
+                })
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(text = "Color",
+                selected = noteOrder is NoteOrder.Color,
+                onSelect = { onOrderChange(NoteOrder.Color(orderType = noteOrder.orderType)
+                )
+                })
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Row(modifier = Modifier.fillMaxWidth()) {
+            DefaultRadioButton(text = "Ascending",
+                selected = noteOrder.orderType is OrderType.Ascending,
+                onSelect = {
+                    onOrderChange(noteOrder.copy(orderType = OrderType.Ascending))
+
+                })
+            Spacer(modifier = Modifier.width(8.dp))
+            DefaultRadioButton(text = "Descending",
+                selected = noteOrder.orderType is OrderType.Descending,
+                onSelect = {
+                    onOrderChange(noteOrder.copy(orderType = OrderType.Descending))
+
+                })
+        }
+    }
+}
